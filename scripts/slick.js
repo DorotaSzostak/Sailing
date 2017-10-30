c2c47ef09c318797605f6252f5fa2d21e7d012d2
dorotaszostak <dorota.szostak@gmail.com>
2017-10-30 17:33:57 +0100

    video section made

---
+++ scripts/slick.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function(){
+    $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  speed: 2000,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: false,
+  arrows: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+  });
+
